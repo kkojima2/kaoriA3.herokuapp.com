ac821182a0036c3dfa9e4bb1981c0e71d24df2cd
Kaori Kojma <90463793+kkojima2@users.noreply.github.com>
2022-03-08 20:58:55 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,123 +1,191 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const exp = require("express");
+const app = exp();
+const sequelize = require("sequelize");
+const handlebars = require("express-handlebars");
+const bodyparser = require("body-parser");
 const multer = require("multer");
-const path = require("path");
 
-app.use(express.static("public"));
-
+// multer requires a few options to be setup to store files with file extensions
+// by default it won't store extensions for security reasons
 const storage = multer.diskStorage({
   destination: "./public/photos/",
   filename: function (req, file, cb) {
+    // we write the filename as the current date down to the millisecond
+    // in a large web service this would possibly cause a problem if two people
+    // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
+    // this is a simple example.
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-let accounts = [];
-
 // tell multer to use the diskStorage function for naming files instead of the default.
 const upload = multer({ storage: storage });
 
-app.get("/", (req, res) => {
-  console.log("go go!");
-  res.sendFile(__dirname + "/home.html");
-});
+app.use(bodyparser.urlencoded({ extended: false }));
 
-// app.get("/abc", (req, res) => {
-//   console.log("go gofdsfdsafdsafsda!");
-//   // res.sendFile(__dirname + "/home.html");
-// });
+app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
-app.post("/registration", upload.single("photo"), (req, res) => {
-  console.log("hello world", req.body);
-
-  console.log("hello this type> ", req.body.fname);
-
-  const { fname, lname, username, password, phone } = req.body;
-
-  let errorMsg = "";
-
-  if (fname == "")
-    errorMsg += `<div class="error-form">Please include first name</div>`;
-  if (lname == "")
-    errorMsg += `<div class="error-form">Please include last name</div>`;
-  if (username == "")
-    errorMsg += `<div class="error-form">Please include username</div>`;
-  if (phone == "")
-    errorMsg += `<div class="error-form">Please include phone number</div>`;
-  if (password == "")
-    errorMsg += `<div class="error-form">Please include password</div>`;
-
-  // if (fname == "")
-  //   errorMsg += `<div class="error-form">Please include first name</div>`;
-
-  if (!/^[a-zA-Z0-9]{6,12}$/.test(username))
-    errorMsg += `<div class="error-form">Username must be between 6 to 12 characters and not include special character</div>`;
-
-  if (!/^\d+$/.test(phone))
-    errorMsg += `<div class="error-form">Phone number must only contain number</div>`;
-
-  if (!/^[a-zA-Z0-9_.-]*$/.test(password))
-    errorMsg += `<div class="error-form">Password must contain letter and numbers only</div>`;
-
-  // console.log("show all data obtained!", { fname, lname, username, password });
-
-  if (!errorMsg) {
-    accounts.push({ fname, lname, username, password, phone });
+const seq_obj = new sequelize(
+  "d859o4uj06kcru", //DATABASE NAME
+  "pgffqaqcyonghm", // USERNAME
+  "d8e1d771e900f2da05d0852c109075304558cd2bddadb90df77da12c59738f7c", // PASSWORD
+  {
+    host: "ec2-34-231-183-74.compute-1.amazonaws.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: { ssl: { rejectUnauthorized: false } },
   }
+);
 
-  res.send(`
-  <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="web322.css" />
+// SQL: create table customers
+const customers = seq_obj.define(
+  "customers3",
+  {
+    id: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: sequelize.STRING,
+    },
+    fname: sequelize.STRING,
+    lname: sequelize.STRING,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
+);
 
-    <title>WEB322</title>
-    <style>
-      .error-form {
-        color: red;
-      }
-    </style>
-  </head>
-  <body>
-    ${errorMsg}
-
-    <!-- <form onSubmit="return sendRequest;"> -->
-    <form action="/registration" method="POST" enctype="multipart/form-data">
-      <label for="fname">First name:</label><br />
-      <input type="text" id="fname" name="fname" value="${fname}" /><br />
-      <label for="lname">Last name:</label><br />
-      <input type="text" id="lname" name="lname" value="${lname}" /><br /><br />
-
-      <label for="phone">Phone:</label><br />
-      <input type="text" id="phone" name="phone" value="${phone}" /><br /><br />
-
-      <label for="username">Username</label><br />
-      <input
-        type="text"
-        id="username"
-        name="username"
-        value="${username}"
-      /><br /><br />
-      <label for="password">Password:</label><br />
-      <input
-        type="password"
-        id="password"
-        name="password"
-        value="${password}"
-      /><br /><br />
-
-      <input type="submit" value="Submit" />
-    </form>
-  </body>
-</html>
-
-  `);
+var Project = seq_obj.define("Project", {
+  title: sequelize.STRING,
+  description: sequelize.TEXT,
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const accounts = seq_obj.define("accounts", {
+  fname: sequelize.STRING,
+  lname: sequelize.STRING,
+  phone: sequelize.INTEGER,
+  username: sequelize.STRING,
+  password: sequelize.STRING,
 });
+
+// { fname, lname, phone, username, password }
+
+seq_obj.sync().then(function () {
+  const port = process.env.PORT || 8080;
+  app.listen(port);
+});
+
+app.get("/", (req, res) => {
+  res.redirect("register");
+});
+
+app.get("/register", (req, res) => {
+  //
+
+  res.render("registration", {
+    data: {},
+    layout: false,
+  });
+});
+
+app.post("/", upload.single("photo"), (req, res) => {
+  console.log("post reuquest setn!", req.body);
+
+  if (req.body["login-btn"]) return res.redirect("/login");
+
+  const { fname, lname, phone, username, password } = req.body;
+
+  let err = [];
+
+  if (!fname) err.push({ msg: "Please include first name" });
+  if (!lname) err.push({ msg: "Please include last name" });
+  if (!phone) err.push({ msg: "Please include phone number" });
+  if (!username) err.push({ msg: "Please include username" });
+  if (!password) err.push({ msg: "Please include password" });
+
+  var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (format.test(username))
+    err.push({ msg: "Username must not contain special character" });
+
+  if (username && (username.length < 6 || username.length > 12))
+    err.push({
+      msg: "Username must be between 6 to 12 characters",
+    });
+
+  if (phone && !/^[0-9]*$/.test(phone))
+    err.push({
+      msg: "Phone number must only contain number",
+    });
+
+  if (!err.length) {
+    // const accounts = seq_obj.define("accounts", {
+    //     fname: sequelize.STRING,
+    //     lname: sequelize.STRING,
+    //     phone: sequelize.NUMBER,
+    //     username: sequelize.STRING,
+    //     password: sequelize.STRING,
+    //   });
+
+    accounts
+      .create({
+        fname,
+        lname,
+        phone: parseInt(phone),
+        username,
+        password,
+      })
+      .then(() => {
+        res.redirect("/login");
+      })
+      .catch(() => {
+        res.render("registration", {
+          data: {
+            error: [{ msg: "Something went wrong!" }],
+            vals: { fname, lname, phone, username, password },
+          },
+          layout: false,
+        });
+      });
+  } else {
+    res.render("registration", {
+      data: { error: err, vals: { fname, lname, phone, username, password } },
+      layout: false,
+    });
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    data: {},
+    layout: false,
+  });
+});
+
+app.post("/login", upload.single("photo"), (req, res) => {
+  console.log("show body: ", req.body);
+
+  if (req.body["register-btn"]) return res.redirect("/register");
+
+  const { username, password } = req.body;
+
+  res.render("login", {
+    data: { vals: { username, password } },
+    layout: false,
+  });
+});
+
+app.get("/dashboard", (req, res) => {});
+
+seq_obj
+  .authenticate()
+  .then(function () {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(function (err) {
+    console.log("Unable to connect to the database:", err);
+  });
